084901c3b9171a634575de335003aa73ec0eefa3
honmanyau <honmanyau@users.noreply.github.com>
2020-12-01 20:34:05 +1100

    Wrote tests and implemented solver for Part 2.

--- 2020/src/Day 1/index.ts
+++ 2020/src/Day 1/index.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 import { green } from '../utilities';
 
 
-export function solvePart1(input: number[], total: number = 2020) {
+export function solverPart1(input: number[], total: number = 2020) {
   for (let i = 0; i < input.length; i++) {
     for (let j = 0; j < input.length; j++) {
       if (
@@ -19,11 +19,30 @@ export function solvePart1(input: number[], total: number = 2020) {
   throw Error('There are no pairs that sum up to 2020 in the input!');
 }
 
+export function solverPart2(input: number[], total: number = 2020) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input.length; j++) {
+      for (let k = 0; k < input.length; k++) {
+        if (
+          i !== j
+          && i !== k
+          && j !== k
+          && (input[i] + input[j] + input[k]) === total
+        ) {
+          return input[i] * input[j] * input[k];
+        }
+      }
+    }
+  }
+
+  throw Error('There are no three numbers that sum up to 2020 in the input!');
+}
+
 if (process.env.SOLVE && process.env.SOLVE.toLowerCase() === 'true') {
   const challengePathname = path.resolve(__dirname, './input.txt');
   const challengeFile = fs.readFileSync(challengePathname, 'utf-8');
   const challenge = challengeFile.trim().split('\n').map(Number);
-  const solution = solvePart1(challenge);
+  const solution = solverPart1(challenge);
 
   console.log([
     `The solutions for 2020's "Day 1: Report Repair" are:`,
--- 2020/src/Day 1/test.ts
+++ 2020/src/Day 1/test.ts
@@ -3,19 +3,19 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 import { yellow } from '../utilities';
-import { solvePart1 } from './index';
+import { solverPart1 as solverPart1, solverPart2 } from './index';
 
 
 const examplePathname = path.resolve(__dirname, './example.txt');
 const exampleFile = fs.readFileSync(examplePathname, 'utf-8');
 const example = exampleFile.trim().split('\n').map(Number);
 
-describe('Day 1: Report Repair — Part 1)', () => {
+describe('Day 1: Report Repair — Part 1', () => {
   it([
     `solvePart1() should return ${yellow(514579)} for the example input:`,
     ` [ ${example.join(' ')} ]`
   ].join(''), () => {
-    const solution = solvePart1(example);
+    const solution = solverPart1(example);
     
     assert.strictEqual(solution, 514579);
   });
@@ -25,7 +25,7 @@ describe('Day 1: Report Repair — Part 1)', () => {
     ` [ 0, 200, 900, 700, 2020 ]`
   ].join(''), () => {
     const testExample = [ 0, 200, 900, 700, 2020 ];
-    const solution = solvePart1(testExample);
+    const solution = solverPart1(testExample);
 
     assert.strictEqual(solution, 0);
   });
@@ -36,7 +36,7 @@ describe('Day 1: Report Repair — Part 1)', () => {
   ].join(''), () => {
     const errorExample = example.filter((val) => val !== 1721);
 
-    assert.throws(() => { solvePart1(errorExample) }, Error);
+    assert.throws(() => { solverPart1(errorExample) }, Error);
   });
 
   it([
@@ -45,7 +45,7 @@ describe('Day 1: Report Repair — Part 1)', () => {
   ].join(''), () => {
     const errorExample = example.filter((val) => val !== 299);
 
-    assert.throws(() => { solvePart1(errorExample) }, Error);
+    assert.throws(() => { solverPart1(errorExample) }, Error);
   });
 
   it([
@@ -54,6 +54,80 @@ describe('Day 1: Report Repair — Part 1)', () => {
   ].join(''), () => {
     const testExample = [ 900, 1010, 1020 ];
 
-    assert.throws(() => { solvePart1(testExample) }, Error);
+    assert.throws(() => { solverPart1(testExample) }, Error);
+  });
+});
+
+describe('Day 1: Report Repair — Part 2', () => {
+  it([
+    `solverPart2() should return ${yellow(241861950)} for the example input:`,
+    ` [ ${example.join(' ')} ]`
+  ].join(''), () => {
+    const solution = solverPart2(example);
+    
+    assert.strictEqual(solution, 241861950);
+  });
+
+  it([
+    `solverPart2() should return ${yellow(0)} for the example input:`,
+    ` [ 0, 2020, 0, 1000, 1010, 900 ]`
+  ].join(''), () => {
+    const testExample = [ 0, 2020, 0, 1000, 1010, 900 ];
+    const solution = solverPart2(testExample);
+    
+    assert.strictEqual(solution, 0);
+  });
+
+  it([
+    `solverPart2() should return ${yellow(0)} for the example input:`,
+    ` [ 0, 1010, 1010, 1000, 2000, 900 ]`
+  ].join(''), () => {
+    const testExample = [ 0, 1010, 1010, 1000, 2000, 900 ];
+    const solution = solverPart2(testExample);
+    
+    assert.strictEqual(solution, 0);
+  });
+
+  it([
+    `should throw an error if for the example input if ${yellow(979)}`,
+    ` is removed from the example arrary: [ ${example.join(' ')} ]`
+  ].join(''), () => {
+    const errorExample = example.filter((val) => val !== 979);
+
+    assert.throws(() => { solverPart2(errorExample) }, Error);
+  });
+
+  it([
+    `should throw an error if for the example input if ${yellow(366)}`,
+    ` is removed from the example arrary: [ ${example.join(' ')} ]`
+  ].join(''), () => {
+    const errorExample = example.filter((val) => val !== 366);
+
+    assert.throws(() => { solverPart2(errorExample) }, Error);
+  });
+
+  it([
+    `should throw an error if for the example input if ${yellow(675)}`,
+    ` is removed from the example arrary: [ ${example.join(' ')} ]`
+  ].join(''), () => {
+    const errorExample = example.filter((val) => val !== 675);
+
+    assert.throws(() => { solverPart2(errorExample) }, Error);
+  });
+
+  it([
+    `should throw an error for the input: [ 0, 2020, 1000, 1010, 900 ]`,
+  ].join(''), () => {
+    const testExample = [ 0, 2020, 1000, 1010, 900 ];
+
+    assert.throws(() => { solverPart2(testExample) }, Error);
+  });
+
+  it([
+    `should throw an error for the input: [ 1010, 1010, 1000, 2000, 900 ]`,
+  ].join(''), () => {
+    const testExample = [ 1010, 1010, 1000, 2000, 900 ];
+
+    assert.throws(() => { solverPart2(testExample) }, Error);
   });
 });
