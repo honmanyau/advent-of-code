5f4215b1a849f5459051bc6868448863861a8380
honmanyau <honmanyau@users.noreply.github.com>
2020-12-02 16:35:06 +1100

    Implemented tests and a mock solver function.

---
+++ 2020/src/day-2/index.ts
@@ -0,0 +1,14 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+import { green } from '../utilities';
+
+
+interface InputEntry {
+  min: number;
+  max: number
+}
+
+export function solver(input: any[]) {
+  return -1;
+}
--- 2020/src/day-2/test.ts
+++ 2020/src/day-2/test.ts
@@ -3,6 +3,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 import { yellow } from '../utilities';
+import { solver } from './index';
 
 
 const examplePathname = path.resolve(__dirname, './example.txt');
@@ -10,8 +11,63 @@ const exampleFile = fs.readFileSync(examplePathname, 'utf-8');
 const example = exampleFile.trim().split('\n').map(processLine);
 
 describe('Day 2: Password Philosophy', () => {
-  it([].join(''), () => {
-    console.log(example);
+  it([
+    `should identify ${yellow(2)} valid passwords in the example input`
+  ].join(''), () => {
+    const solution = solver(example);
+
+    assert.strictEqual(solution, 2);
+  });
+
+  it([
+    `should identify ${yellow(2)} valid passwords in the example input if the`,
+    ' second entry is removed.'
+  ].join(''), () => {
+    const modifiedExample = [ ...example ].filter((_v, i) => i !== 1);
+    const solution = solver(modifiedExample);
+
+    assert.strictEqual(solution, 2);
+  });
+
+  it([
+    `should identify ${yellow(1)} valid passwords in the example input if the`,
+    ' first entry is removed.'
+  ].join(''), () => {
+    const modifiedExample = [ ...example ].filter((_v, i) => i !== 0);
+    const solution = solver(modifiedExample);
+
+    assert.strictEqual(solution, 1);
+  });
+
+  it([
+    `should identify ${yellow(1)} valid passwords in the example input if the`,
+    ' last entry is removed.'
+  ].join(''), () => {
+    const modifiedExample = [ ...example ].filter((_v, i) => i !== 2);
+    const solution = solver(modifiedExample);
+
+    assert.strictEqual(solution, 1);
+  });
+
+  it([
+    `should identify ${yellow(0)} valid passwords in the example input if the`,
+    ' first and last last entry are removed.'
+  ].join(''), () => {
+    const modifiedExample = [ ...example ].filter((_v, i) => i === 1);
+    const solution = solver(modifiedExample);
+
+    assert.strictEqual(solution, 0);
+  });
+
+  it([
+    `should throw an error if value of the \`max\` property of an entry`,
+    ` is less than the value of the \`min\` property.`
+  ].join(''), () => {
+    const testInput = [
+      { min: 2, max: 1, letter: 'a', password: 'abcde' }
+    ];
+
+    assert.throws(() => solver(testInput), Error);
   });
 });
 
