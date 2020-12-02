2de31b2acf303f885666387d1574069b1d035812
honmanyau <honmanyau@users.noreply.github.com>
2020-12-02 16:39:26 +1100

    Completed interface for input entries. Implemented throw for when min > max.

--- 2020/src/day-2/index.ts
+++ 2020/src/day-2/index.ts
@@ -4,11 +4,21 @@ import * as path from 'path';
 import { green } from '../utilities';
 
 
-interface InputEntry {
+interface Entry {
   min: number;
-  max: number
+  max: number;
+  letter: string;
+  password: string;
 }
 
-export function solver(input: any[]) {
+export function solver(input: Entry[]) {
+  for (const entry of input) {
+    const { min, max, letter, password } = entry;
+
+    if (min > max) {
+      throw Error('An entry with `min` > `max` was provided!');
+    }
+  }
+
   return -1;
 }
