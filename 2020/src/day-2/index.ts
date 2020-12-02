ca8475998f5ac9d84f9cf0c55e1988c02198f344
honmanyau <honmanyau@users.noreply.github.com>
2020-12-02 16:53:29 +1100

    Implemented solver function. Tests passing.

--- 2020/src/day-2/index.ts
+++ 2020/src/day-2/index.ts
@@ -12,13 +12,28 @@ interface Entry {
 }
 
 export function solver(input: Entry[]) {
+  let numValidPasswords = 0;
+
   for (const entry of input) {
     const { min, max, letter, password } = entry;
 
     if (min > max) {
       throw Error('An entry with `min` > `max` was provided!');
     }
+
+    const re = new RegExp(letter, 'g');
+    const matched = password.match(re);
+    const numLettersFound = matched ? matched.length : 0;
+    const isValidPassword = numLettersFound >= min
+      && numLettersFound <= max;
+
+    if (
+      numLettersFound >= min
+      && numLettersFound <= max
+    ) {
+      numValidPasswords += 1;
+    }
   }
 
-  return -1;
+  return numValidPasswords;
 }
