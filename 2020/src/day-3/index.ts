ad109ff0f33bd97422ef9305b5d0e401f41df24d
honmanyau <honmanyau@users.noreply.github.com>
2020-12-03 16:25:13 +1100

    Advent of Code 2020, Day 3 project setup.

---
+++ 2020/src/day-3/index.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+import { green } from '../utilities';
+
+
+if (process.env.SOLVE && process.env.SOLVE.toLowerCase() === 'true') {
+  const challengePathname = path.resolve(__dirname, './input.txt');
+  const challengeFile = fs.readFileSync(challengePathname, 'utf-8');
+  const challenge = processFile(challengeFile);
+  const solutionPart1 = solverPart1(challenge);
+
+  console.log([
+    `The solutions for 2020's "Day 3: Toboggan Trajectory" are:`,
+    `  * Part 1: ${green(solutionPart1)}`
+  ].join('\n'));
+
+  
+}
+
+// ===============
+// == Functions ==
+// ===============
+/**
+ * This function uses the `processLine` function process an input file of
+ * the Advent of Code 2020's "Day 2: Password Philosophy" challenge.
+ * @param {string} file A challenge file read in as a string.
+ * @returns {Entry[]} An array where each line is an entry of the challenge.
+ */
+export function processFile(file: string) {
+  return file.trim().split('\n');
+}
+
+/**
+ * The solver function for Part 1 of the Advent of Code 2020's
+ * "Day 2: Password Philosophy" challenge.
+ * @param {Entry[]} input Entries of the challenge.
+ * @returns {number} Number of valid entries.
+ */
+export function solverPart1(input: string[]) {
+  let numTreesEncountered = -1;
+
+  return -1;
+}
---
+++ 2020/src/day-3/test.ts
@@ -0,0 +1,19 @@
+import * as assert from 'assert';
+import * as fs from 'fs';
+import * as path from 'path';
+
+import { yellow } from '../utilities';
+import { processFile, solverPart1 } from './index';
+
+
+const examplePathname = path.resolve(__dirname, './example.txt');
+const exampleFile = fs.readFileSync(examplePathname, 'utf-8');
+const example = processFile(exampleFile);
+
+describe('Day 3: Toboggan Trajectory', () => {
+  it([
+    ''
+  ].join(''), () => {
+
+  });
+});
