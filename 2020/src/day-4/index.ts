82a7cb31078b835601f668a07bcef3023d1af219
honmanyau <honmanyau@users.noreply.github.com>
2020-12-04 17:03:06 +1100

    Advent of Code, Day 4, project setup.

---
+++ 2020/src/day-4/index.ts
@@ -0,0 +1,40 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+import { green } from '../utilities';
+
+
+if (process.env.SOLVE && process.env.SOLVE.toLowerCase() === 'true') {
+  const challengePathname = path.resolve(__dirname, './input.txt');
+  const challengeFile = fs.readFileSync(challengePathname, 'utf-8');
+  const challenge = processFile(challengeFile);
+  const solutionPart1 = solverPart1(challenge);
+
+  console.log([
+    `The solutions for 2020's "Day 4: Passport Processing" are:`,
+    `  * Part 1: ${green(solutionPart1)}`
+  ].join('\n'));
+}
+
+// ===============
+// == Functions ==
+// ===============
+/**
+ * This function uses the `processLine` function process an input file of
+ * the Advent of Code 2020's "Day 4: Passport Processing" challenge.
+ * @param {string} file A challenge file read in as a string.
+ * @returns {string[]} An array where each line is an entry of the challenge.
+ */
+export function processFile(file: string) {
+  return file.trim().split('\n');
+}
+
+/**
+ * The solver function for Part 1 of the Advent of Code 2020's
+ * "Day 4: Passport Processing" challenge.
+ * @param {string[]} input Entries of the challenge.
+ * @returns {number} Number of valid entries.
+ */
+export function solverPart1(input: string[]) {
+  return -1;
+}
