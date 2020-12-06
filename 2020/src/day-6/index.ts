b63d2e2305fc99f85fe271ad4450c61a2f0a8dca
honmanyau <honmanyau@users.noreply.github.com>
2020-12-06 16:20:09 +1100

    Added tests for counting the number of unique yes answers in a group. Mocked function.

--- 2020/src/day-6/index.ts
+++ 2020/src/day-6/index.ts
@@ -8,11 +8,11 @@ if (process.env.SOLVE && process.env.SOLVE.toLowerCase() === 'true') {
   const challengePathname = path.resolve(__dirname, './input.txt');
   const challengeFile = fs.readFileSync(challengePathname, 'utf-8');
   const challenge = processFile(challengeFile);
-  const solutionPart1 = solverPart1(challenge);
+  // const solutionPart1 = solverPart1(challenge);
 
   console.log([
-    `The solutions for 2020's "Day X: XXXXXXX" are:`,
-    `  * Part 1: ${green(solutionPart1)}`
+    `The solutions for 2020's "Day 6: Custom Customs" are:`,
+    // `  * Part 1: ${green(solutionPart1)}`
   ].join('\n'));
 }
 
@@ -26,18 +26,38 @@ if (process.env.SOLVE && process.env.SOLVE.toLowerCase() === 'true') {
 // == Functions ==
 // ===============
 /**
- * This function uses the `processLine` function process an input file of
- * the Advent of Code 2020's "Day X: XXXXXXX" challenge.
+ * This function creates a dictionary for the questions that people in a group
+ * have answered yes.
+ * @param {string} input The answers for a given group.
+ * @returns The number of unique answers that people have answered yes to. 
+ */
+export function countUniqueYeses(input: string[]) {
+  return -1;
+}
+
+/**
+ * This function uses the `processEntry` function process an input file of
+ * the Advent of Code 2020's "Day 6: Custom Customs" challenge.
  * @param {string} file A challenge file read in as a string.
- * @returns {string[]} An array where each line is an entry of the challenge.
+ * @returns {string[][]} An array where each line is an entry of the challenge.
  */
 export function processFile(file: string) {
-  return file.trim().split('\n');
+  return file.trim().split('\n\n').map(processEntry);
+}
+
+/**
+ * This function processes each entry of pre-processed input.
+ * the Advent of Code 2020's "Day 6: Custom Customs" challenge.
+ * @param {string[]} file A challenge file read in as a string.
+ * @returns {string[]} An array where each line is an entry of the challenge.
+ */
+export function processEntry(entry: string) {
+  return entry.split('\n');
 }
 
 /**
  * The solver function for Part 1 of the Advent of Code 2020's
- * "Day X: XXXXXXX" challenge.
+ * "Day 6: Custom Customs" challenge.
  * @param {any} input Entries of the challenge.
  * @returns {number} Number of valid entries.
  */
@@ -47,7 +67,7 @@ export function solverPart1(input: string[]) {
 
 /**
  * The solver function for Part 2 of the Advent of Code 2020's
- * "Day X: XXXXXXX" challenge.
+ * "Day 6: Custom Customs" challenge.
  * @param {any} input Entries of the challenge.
  * @returns {number} Number of valid entries.
  */
--- 2020/src/day-6/test.ts
+++ 2020/src/day-6/test.ts
@@ -5,9 +5,7 @@ import * as path from 'path';
 import { yellow } from '../utilities';
 import {
   processFile,
-  getRowNumber,
-  getColNumber,
-  getSeatId,
+  countUniqueYeses,
   solverPart1
 } from './index';
 
@@ -16,12 +14,109 @@ const examplePathname = path.resolve(__dirname, './example.txt');
 const exampleFile = fs.readFileSync(examplePathname, 'utf-8');
 const example = processFile(exampleFile);
 
-describe('Day X: XXXXXXX (Part 1)', () => {
-  describe('someFunction()', () => {
+console.log(example);
+
+describe('Day 6: Custom Customs (Part 1)', () => {
+  describe('countUniqueYeses()', () => {
+    // Given example.
     it([
-      `should so something`
+      `should count ${yellow(1)} for the input [ 'a' ]`
     ].join(''), () => {
-      //
+      const count = countUniqueYeses([ 'a' ]);
+
+      assert.strictEqual(count, 1);
+    });
+
+    it([
+      `should count ${yellow(2)} for the input [ 'ab' ]`
+    ].join(''), () => {
+      const count = countUniqueYeses([ 'ab' ]);
+
+      assert.strictEqual(count, 2);
+    });
+
+    it([
+      `should count ${yellow(3)} for the input [ 'abc' ]`
+    ].join(''), () => {
+      const count = countUniqueYeses([ 'abc' ]);
+
+      assert.strictEqual(count, 3);
+    });
+
+    it([
+      `should count ${yellow(2)} for the input [ 'a', 'ab' ]`
+    ].join(''), () => {
+      const count = countUniqueYeses([ 'a', 'ab' ]);
+
+      assert.strictEqual(count, 2);
+    });
+
+    it([
+      `should count ${yellow(3)} for the input [ 'a', 'abc' ]`
+    ].join(''), () => {
+      const count = countUniqueYeses([ 'a', 'abc' ]);
+
+      assert.strictEqual(count, 3);
+    });
+
+    it([
+      `should count ${yellow(3)} for the input [ 'ab', 'abc' ]`
+    ].join(''), () => {
+      const count = countUniqueYeses([ 'ab', 'abc' ]);
+
+      assert.strictEqual(count, 3);
+    });
+
+    it([
+      `should count ${yellow(3)} for the input [ 'abc', 'abc' ]`
+    ].join(''), () => {
+      const count = countUniqueYeses([ 'abc', 'abc' ]);
+
+      assert.strictEqual(count, 3);
+    });
+
+    describe('countUniqueYeses()', () => {
+      // Given example.
+      it([
+        `should count ${yellow(3)} for the input [ 'a', 'b', 'c' ]`
+      ].join(''), () => {
+        const count = countUniqueYeses([ 'a', 'b', 'c' ]);
+  
+        assert.strictEqual(count, 3);
+      });
+    });
+
+    describe('countUniqueYeses()', () => {
+      // Given example.
+      it([
+        `should count ${yellow(3)} for the input [ 'ab', 'ac' ]`
+      ].join(''), () => {
+        const count = countUniqueYeses([ 'ab', 'ac' ]);
+  
+        assert.strictEqual(count, 3);
+      });
+    });
+
+    describe('countUniqueYeses()', () => {
+      // Given example.
+      it([
+        `should count ${yellow(1)} for the input [ 'a', 'a', 'a', 'a' ]`
+      ].join(''), () => {
+        const count = countUniqueYeses([ 'a', 'a', 'a', 'a' ]);
+  
+        assert.strictEqual(count, 1);
+      });
+    });
+
+    describe('countUniqueYeses()', () => {
+      // Given example.
+      it([
+        `should count ${yellow(1)} for the input [ 'b' ]`
+      ].join(''), () => {
+        const count = countUniqueYeses([ 'b' ]);
+  
+        assert.strictEqual(count, 1);
+      });
     });
   });
 });
