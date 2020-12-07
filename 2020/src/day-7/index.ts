5cb1eb52b71ca4ab021ec1b8f475aa06d1d9ffc6
honmanyau <honmanyau@users.noreply.github.com>
2020-12-07 23:53:46 +1100

    Solved Part 2. Solution accepted, but clearly taking a couple of seconds too long.

--- 2020/src/day-7/index.ts
+++ 2020/src/day-7/index.ts
@@ -9,12 +9,12 @@ if (process.env.SOLVE && process.env.SOLVE.toLowerCase() === 'true') {
   const challengeFile = fs.readFileSync(challengePathname, 'utf-8');
   const challenge = processFile(challengeFile);
   const solutionPart1 = solverPart1(challenge);
-  // const solutionPart2 = solverPart2(challenge);
+  const solutionPart2 = solverPart2(challenge);
 
   console.log([
     `The solutions for 2020's "Day 7: Handy Haversacks" are:`,
     `  * Part 1: ${green(solutionPart1)}`,
-    // `  * Part 2: ${green(solutionPart2)}`
+    `  * Part 2: ${green(solutionPart2)}`
   ].join('\n'));
 }
 
