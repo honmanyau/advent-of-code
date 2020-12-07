b8bd281bb9b107390f4900779a96ec9f496297f4
honmanyau <honmanyau@users.noreply.github.com>
2020-12-07 21:36:17 +1100

    Added tests for parsing entries in the example.

--- 2020/src/day-7/test.ts
+++ 2020/src/day-7/test.ts
@@ -4,6 +4,7 @@ import * as path from 'path';
 
 import { yellow } from '../utilities';
 import {
+  processEntry,
   processFile,
   solverPart1
 } from './index';
@@ -14,12 +15,102 @@ const exampleFile = fs.readFileSync(examplePathname, 'utf-8');
 const example = processFile(exampleFile);
 
 describe('Day 7: Handy Haversacks (Part 1)', () => {
-  describe('someFunction()', () => {
+  describe('processEntry()', () => {
     // Given example.
     it([
-      `should do something.`
+      `should parse "light red bags contain 1 bright white bag, 2 muted yellow`,
+      ` bags." to give the object:\n`,
+      `        {\n`,
+      `          name: 'light red'\n`,
+      `          content: [\n`,
+      `            { name: 'bright white', amount: 1 },\n`,
+      `            { name: 'muted yellow', amount: 2 }\n`,
+      `          ]\n`,
+      `        }`
     ].join(''), () => {
-      
+      const testString =
+        'light red bags contain 1 bright white bag, 2 muted yellow bags.';
+      const entry = processEntry(testString);
+      const expectedEntry = {
+        name: 'light red',
+        content: [
+          { name: 'bright white', amount: 1 },
+          { name: 'muted yellow', amount: 2 }
+        ]
+      };
+
+      assert.deepStrictEqual(entry, expectedEntry);
+    });
+
+    // Given example.
+    it([
+      `should parse "vibrant plum bags contain 5 faded blue bags, 6 dotted`,
+      ` black bags." to give the object:\n`,
+      `        {\n`,
+      `          name: 'vibrant plum'\n`,
+      `          content: [\n`,
+      `            { name: 'faded blue', amount: 5 },\n`,
+      `            { name: 'dotted black', amount: 6 }\n`,
+      `          ]\n`,
+      `        }`
+    ].join(''), () => {
+      const testString =
+        'vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.';
+      const entry = processEntry(testString);
+      const expectedEntry = {
+        name: 'vibrant plum',
+        content: [
+          { name: 'faded blue', amount: 5 },
+          { name: 'dotted black', amount: 6 }
+        ]
+      };
+
+      assert.deepStrictEqual(entry, expectedEntry);
+    });
+
+    // Given example.
+    it([
+      `should parse "bright white bags contain 1 shiny gold bag.`,
+      ` to give the object:\n`,
+      `        {\n`,
+      `          name: 'bright white'\n`,
+      `          content: [\n`,
+      `            { name: 'shiny gold', amount: 1 }\n`,
+      `          ]\n`,
+      `        }`
+    ].join(''), () => {
+      const testString =
+        'bright white bags contain 1 shiny gold bag.';
+      const entry = processEntry(testString);
+      const expectedEntry = {
+        name: 'bright white',
+        content: [
+          { name: 'shiny gold', amount: 1 }
+        ]
+      };
+
+      assert.deepStrictEqual(entry, expectedEntry);
+    });
+
+
+    // Given example.
+    it([
+      `should parse "faded blue bags contain no other bags.`,
+      ` to give the object:\n`,
+      `        {\n`,
+      `          name: 'faded blue'\n`,
+      `          content: []\n`,
+      `        }`
+    ].join(''), () => {
+      const testString =
+        'faded blue bags contain no other bags.';
+      const entry = processEntry(testString);
+      const expectedEntry = {
+        name: 'faded blue',
+        content: []
+      };
+
+      assert.deepStrictEqual(entry, expectedEntry);
     });
   });
 });
