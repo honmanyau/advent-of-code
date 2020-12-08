a1b902a9ea9c4a9582166972e550f73e31d1da4b
honmanyau <honmanyau@users.noreply.github.com>
2020-12-08 15:56:06 +1100

    Added project template.

---
+++ 2020/src/template/index.ts
@@ -0,0 +1,89 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+import { green } from '../utilities';
+
+
+if (process.env.SOLVE && process.env.SOLVE.toLowerCase() === 'true') {
+  const challengePathname = path.resolve(__dirname, './input.txt');
+  const challengeFile = fs.readFileSync(challengePathname, 'utf-8');
+  const challenge = processFile(challengeFile);
+  const solutionPart1 = solverPart1(challenge);
+  const solutionPart2 = logExecutionTime(
+    'solverPart2',
+    () => solverPart2(challenge)
+  );
+  const solutionPart2Attempt2 = logExecutionTime(
+    'solverPart2Attempt2',
+    () => solverPart2Attempt2(challenge)
+  );
+
+  console.log([
+    `The solutions for 2020's "Day 7: Handy Haversacks" are:`,
+    `  * Part 1: ${green(solutionPart1)}`,
+    `  * Part 2: ${green(solutionPart2)}`,
+    `  * Part 2 (Attempt 2): ${green(solutionPart2Attempt2)}`
+  ].join('\n'));
+}
+
+
+// ================
+// == Interfaces ==
+// ================
+interface Bags {
+  [name: string]: Bag
+}
+
+interface Bag {
+  name: string;
+  content: ContentBag[];
+}
+
+interface ContentBag {
+  name: string;
+  amount: number;
+}
+
+
+// ===============
+// == Functions ==
+// ===============
+/**
+ * This function uses the `processEntry` function process an input file of
+ * the Advent of Code 2020's "Day 7: Handy Haversacks" challenge.
+ * @param {string} file A challenge file read in as a string.
+ * @returns {string[]} An array where each line is an entry of the challenge.
+ */
+export function processFile(file: string) {
+  return file.trim().split('\n').map(processEntry);
+}
+
+/**
+ * This function processes each entry of pre-processed input.
+ * the Advent of Code 2020's "Day 7: Handy Haversacks" challenge.
+ * @param {string} file A challenge file read in as a string.
+ * @returns {string} An array where each line is an entry of the challenge.
+ */
+export function processEntry(entry: string) {
+  return entry;
+}
+
+/**
+ * The solver function for Part 1 of the Advent of Code 2020's
+ * "Day 7: Handy Haversacks" challenge.
+ * @param {string[]} input Entries of the challenge.
+ * @returns {number} Number of valid entries.
+ */
+export function solverPart1(input: string[]) {
+  return -1;
+}
+
+/**
+ * The solver function for Part 2 of the Advent of Code 2020's
+ * "Day 7: Handy Haversacks" challenge.
+ * @param {string[]} input Entries of the challenge.
+ * @returns {number} Number of valid entries.
+ */
+export function solverPart2(input: string[]) {
+  return -1;
+}
