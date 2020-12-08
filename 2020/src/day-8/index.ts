41d0cfb30d8f50e26f9e2b31c5f0a53d4191b6a5
honmanyau <honmanyau@users.noreply.github.com>
2020-12-08 18:27:29 +1100

    Implemented walk().

--- 2020/src/day-8/index.ts
+++ 2020/src/day-8/index.ts
@@ -36,6 +36,16 @@ interface ContentBag {
   amount: number;
 }
 
+interface WalkResults {
+  visitedPath: number[];
+  visitedNop: number[];
+  visitedJmp: number[];
+  looped: boolean;
+  outOfRange: boolean;
+  terminated: boolean;
+  accumulator: number;
+}
+
 type Instruction = 'nop' | 'acc' | 'jmp' | '---';
 type Entry = [ Instruction, number ];
 
@@ -82,7 +92,7 @@ export function solverPart1(instructions: Entry[]) {
     const entry = instructions[currentPosition];
 
     if (!entry) {
-      throw Error('Out of range: there is no infinite loop.');
+      throw Error('Out of range!');
     }
 
     const [ instruction, arg ] = entry;
@@ -120,9 +130,136 @@ export function solverPart1(instructions: Entry[]) {
 /**
  * The solver function for Part 2 of the Advent of Code 2020's
  * "Day 8: Handheld Halting" challenge.
- * @param {string[]} input Entries of the challenge.
+ * @param {Entry[]} input Entries of the challenge.
  * @returns {number} Number of valid entries.
  */
-export function solverPart2(istructions: Entry[]) {
-  return -1E16;
+export function solverPart2(instructions: Entry[]) {
+  const visited = {};
+  const loopedPath = [];
+  const visitedNop = [];
+  const visitedJmp = [];
+  let accumulator = 0;
+  let currentPosition = 0;
+
+  // Find infinite path.
+  let looped = false;
+
+  while (!looped) {
+    const entry = instructions[currentPosition];
+
+    if (!entry) {
+      throw Error('Out of range!');
+    }
+
+    const [ instruction, arg ] = entry;
+
+    if (visited[currentPosition]) {
+      looped = true;
+      break;
+    }
+    else {
+      visited[currentPosition] = true;
+      loopedPath.push(currentPosition);
+
+      switch(instruction) {
+        case 'nop':
+          currentPosition += 1;
+          visitedNop.push(currentPosition);
+
+          break;
+        case 'acc':
+          currentPosition += 1;
+          accumulator += arg;
+
+          break;
+        case 'jmp':
+          currentPosition += arg;
+          visitedJmp.push(currentPosition);
+
+          break;
+        default:
+          throw Error('The program should never get to this point!');
+      }
+    }
+  }
+
+  return accumulator;
+}
+
+/**
+ * This functions walks through the instructions given and returns details
+ * about the path walked.
+ * @param {Entry[]} input Entries of the challenge.
+ * @returns {number} Number of valid entries.
+ */
+export function walk(instructions: Entry[]): WalkResults {
+  const visited = {};
+  const visitedPath = [];
+  const visitedNop = [];
+  const visitedJmp = [];
+  let accumulator = 0;
+  let currentPosition = 0;
+  let nextPosition = currentPosition;
+  let looped = false;
+  let outOfRange = false;
+  let terminated = false;
+
+  // Find infinite path.
+
+  while (!looped && !outOfRange && !terminated) {
+    const entry = instructions[currentPosition];
+
+    if (!entry) {
+      outOfRange = true;
+      break;
+    }
+
+    const [ instruction, arg ] = entry;
+
+    visitedPath.push(currentPosition);
+
+    if (currentPosition === instructions.length - 1) {
+      if (instruction === 'nop' || instruction === 'acc') {
+        terminated = true;
+      }
+    }
+
+    if (visited[currentPosition]) {
+      looped = true;
+    }
+    else {
+      switch(instruction) {
+        case 'nop':
+          visitedNop.push(currentPosition);
+          nextPosition = currentPosition + 1;
+  
+          break;
+        case 'acc':
+          nextPosition = currentPosition + 1;
+          accumulator += arg;
+  
+          break;
+        case 'jmp':
+          visitedJmp.push(currentPosition);
+          nextPosition = currentPosition + arg;
+  
+          break;
+        default:
+          throw Error('The program should never get to this point!');
+      }
+    }
+
+    visited[currentPosition] = true;
+    currentPosition = nextPosition;
+  }
+
+  return {
+    visitedPath,
+    visitedNop,
+    visitedJmp,
+    looped,
+    outOfRange,
+    terminated,
+    accumulator
+  };
 }
