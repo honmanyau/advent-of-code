df4f5d56c39884e57b9a5643cbe81a70e8457d58
honmanyau <honmanyau@users.noreply.github.com>
2020-12-09 16:39:23 +1100

    Added tests for finding the first weakness in XMAS data. Mocked the corresponding functions.

--- 2020/src/day-9/index.ts
+++ 2020/src/day-9/index.ts
@@ -41,12 +41,22 @@ interface ContentBag {
 // == Functions ==
 // ===============
 /**
+ * This fucntion returns the first weakness found in the XMAS data according to
+ * the rules specified in the challenge.
+ * @param {number[]} data The XMAS data.
+ * @param {number} preambleSize The size of the preamble.
+ */
+export function findWeakness(data: number[], preambleSize: number = 25) {
+  return -1;
+}
+
+/**
  * This function uses the `processEntry` function process an input file of
  * the Advent of Code 2020's "Day 9: Encoding Error" challenge.
  * @param {string} file A challenge file read in as a string.
- * @returns {string[]} An array where each line is an entry of the challenge.
+ * @returns {number[]} An array where each line is an entry of the challenge.
  */
-export function processFile(file: string) {
+export function processFile(file: string): number[] {
   return file.trim().split('\n').map(processEntry);
 }
 
@@ -54,28 +64,28 @@ export function processFile(file: string) {
  * This function processes each entry of pre-processed input.
  * the Advent of Code 2020's "Day 9: Encoding Error" challenge.
  * @param {string} file A challenge file read in as a string.
- * @returns {string} An array where each line is an entry of the challenge.
+ * @returns {number} An array where each line is an entry of the challenge.
  */
-export function processEntry(entry: string) {
-  return entry;
+export function processEntry(entry: string): number {
+  return Number(entry);
 }
 
 /**
  * The solver function for Part 1 of the Advent of Code 2020's
  * "Day 9: Encoding Error" challenge.
- * @param {string[]} input Entries of the challenge.
+ * @param {number[]} input Entries of the challenge.
  * @returns {number} Number of valid entries.
  */
-export function solverPart1(input: string[]) {
+export function solverPart1(input: number[]) {
   return -1;
 }
 
 /**
  * The solver function for Part 2 of the Advent of Code 2020's
  * "Day 9: Encoding Error" challenge.
- * @param {string[]} input Entries of the challenge.
+ * @param {number[]} input Entries of the challenge.
  * @returns {number} Number of valid entries.
  */
-export function solverPart2(input: string[]) {
+export function solverPart2(input: number[]) {
   return -1;
 }
--- 2020/src/day-9/test.ts
+++ 2020/src/day-9/test.ts
@@ -5,6 +5,7 @@ import * as path from 'path';
 import { yellow } from '../utilities';
 import {
   processFile,
+  findWeakness,
   solverPart1,
   solverPart2
 } from './index';
@@ -15,6 +16,56 @@ const exampleFile = fs.readFileSync(examplePathname, 'utf-8');
 const example = processFile(exampleFile);
 
 describe('Day 9: Encoding Error (Part 1)', () => {
+  describe([
+    `findWeakness()`
+  ].join(''), () => {
+    it([
+      `should return ${yellow(127)} for the example data with a preamble size`,
+      ` of ${yellow(5)}.`
+    ].join(''), () => {
+      const weakness = findWeakness(example, 5);
+
+      assert.strictEqual(weakness, 127);
+    });
+
+    it([
+      `should return ${yellow(1024)} for the example data with a preamble`,
+      ` size of ${yellow(5)} if the sixth entry is changed to 1024.`
+    ].join(''), () => {
+      const modifiedData = [ ...example ];
+
+      modifiedData[5] = 1024;
+
+      const weakness = findWeakness(modifiedData, 5);
+
+      assert.strictEqual(weakness, 1024);
+    });
+
+    it([
+      `should return ${yellow(2048)} for the example data with a preamble`,
+      ` size of ${yellow(5)} if the 15th entry is changed to 332 (valid) and.`,
+      ` the 16th entry is changed to 2048.`,
+    ].join(''), () => {
+      const modifiedData = [ ...example ];
+
+      modifiedData[14] = 332;
+      modifiedData[15] = 2048;
+
+      const weakness = findWeakness(modifiedData, 5);
+
+      assert.strictEqual(weakness, 2048);
+    });
+
+    it([
+      `should throw an error if the 15th entry and all entries that follow are`,
+      ` removed.`
+    ].join(''), () => {
+      const modifiedData = example.slice(0, 14);
+
+      assert.throws(() => findWeakness(modifiedData, 5), Error);
+    });
+  });
+
   describe(`sovlerPart1()`, () => {
     it([
       `should do something.`
