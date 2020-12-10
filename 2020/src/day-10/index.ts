ae2224d74e3844efbeae3c29ee8b63febf92ff36
honmanyau <honmanyau@users.noreply.github.com>
2020-12-10 16:58:30 +1100

    Implmeneted calcDifferences.

--- 2020/src/day-10/index.ts
+++ 2020/src/day-10/index.ts
@@ -41,41 +41,56 @@ interface ContentBag {
 // == Functions ==
 // ===============
 /**
- * This function uses the `processEntry` function process an input file of
- * the Advent of Code 2020's "Day 10: Adapter Array" challenge.
+ * This function processes an input file of the Advent of Code 2020's
+ * "Day 10: Adapter Array" challenge.
  * @param {string} file A challenge file read in as a string.
  * @returns {string[]} An array where each line is an entry of the challenge.
  */
 export function processFile(file: string) {
-  return file.trim().split('\n').map(processEntry);
+  return file.trim().split('\n').map(Number);
 }
 
 /**
- * This function processes each entry of pre-processed input.
- * the Advent of Code 2020's "Day 10: Adapter Array" challenge.
- * @param {string} file A challenge file read in as a string.
- * @returns {string} An array where each line is an entry of the challenge.
+ * This function sorts a list of adapter joltages, appends the voltage of the
+ * device, and returns the differences between them.
+ * @param {number[]} adapters A list of adapter joltages.
+ * @returns A list of joltage differences.
  */
-export function processEntry(entry: string) {
-  return entry;
+export function calcDifferences(adapters: number[]) {
+  const allJoltagesSorted = [ ...adapters ].sort((a, b) => a - b);
+
+  allJoltagesSorted.push(allJoltagesSorted[allJoltagesSorted.length - 1] + 3);
+
+  const differences = allJoltagesSorted.map((joltage, i) => {
+    const prevJoltage = allJoltagesSorted[i - 1] || 0;
+    const difference = joltage - prevJoltage;
+
+    if (difference <= 0 || difference >= 4) {
+      throw Error('Invalid solution found!');
+    }
+
+    return joltage - prevJoltage;
+  });
+
+  return differences;
 }
 
 /**
  * The solver function for Part 1 of the Advent of Code 2020's
  * "Day 10: Adapter Array" challenge.
- * @param {string[]} input Entries of the challenge.
+ * @param {number[]} adapters Entries of the challenge.
  * @returns {number} Number of valid entries.
  */
-export function solverPart1(input: string[]) {
+export function solverPart1(adapters: number[]) {
   return -1;
 }
 
 /**
  * The solver function for Part 2 of the Advent of Code 2020's
  * "Day 10: Adapter Array" challenge.
- * @param {string[]} input Entries of the challenge.
+ * @param {number[]} input Entries of the challenge.
  * @returns {number} Number of valid entries.
  */
-export function solverPart2(input: string[]) {
+export function solverPart2(adapters: number[]) {
   return -1;
 }
