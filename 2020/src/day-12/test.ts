f1297e8f8d43ef83c2db3865fc52188cad1449c1
honmanyau <honmanyau@users.noreply.github.com>
2020-12-13 22:50:59 +1100

    Added tests for solverPart2().

--- 2020/src/day-12/test.ts
+++ 2020/src/day-12/test.ts
@@ -6,7 +6,8 @@ import { yellow } from '../utilities';
 import {
   processFile,
   solverPart1,
-  solverPart2
+  solverPart2,
+  Instruction
 } from './index';
 
 
@@ -17,9 +18,139 @@ const example = processFile(exampleFile);
 describe('Day 12: Rain Risk (Part 1)', () => {
   describe(`sovlerPart1()`, () => {
     it([
-      `should do something.`
+      `should return 25 for the example.`
     ].join(''), () => {
       const solution = solverPart1(example);
+
+      assert.strictEqual(solution, 25);
+    });
+
+    it([
+      `should return 0 for the following test input:`,
+      '          [ F 0 ]'
+    ].join(''), () => {
+      const testInput: Instruction[] = [
+        [ 'F', 0 ]
+      ];
+      const solution = solverPart1(testInput);
+
+      assert.strictEqual(solution, 0);
+    });
+
+    it([
+      `should return 10 for the following test input:`,
+      '          [ F 21 ]'
+    ].join(''), () => {
+      const testInput: Instruction[] = [
+        [ 'F', 21 ]
+      ];
+      const solution = solverPart1(testInput);
+
+      assert.strictEqual(solution, 10);
+    });
+    
+    it([
+      `should return 42 for the following test input:`,
+      '          [ E 21 ]',
+      '          [ S 21 ]'
+    ].join(''), () => {
+      const testInput: Instruction[] = [
+        [ 'E', 21 ],
+        [ 'S', 21 ]
+      ];
+      const solution = solverPart1(testInput);
+
+      assert.strictEqual(solution, 42);
+    });
+
+    it([
+      `should return 42 for the following test input:`,
+      '          [ E 21 ]',
+      '          [ N 21 ]'
+    ].join(''), () => {
+      const testInput: Instruction[] = [
+        [ 'E', 21 ],
+        [ 'N', 21 ]
+      ];
+      const solution = solverPart1(testInput);
+
+      assert.strictEqual(solution, 42);
+    });
+
+    it([
+      `should return 42 for the following test input:`,
+      '          [ W 21 ]',
+      '          [ S 21 ]'
+    ].join(''), () => {
+      const testInput: Instruction[] = [
+        [ 'W', 21 ],
+        [ 'S', 21 ]
+      ];
+      const solution = solverPart1(testInput);
+
+      assert.strictEqual(solution, 42);
+    });
+
+    it([
+      `should return 42 for the following test input:`,
+      '          [ W 21 ]',
+      '          [ N 21 ]'
+    ].join(''), () => {
+      const testInput: Instruction[] = [
+        [ 'W', 21 ],
+        [ 'N', 21 ]
+      ];
+      const solution = solverPart1(testInput);
+
+      assert.strictEqual(solution, 42);
+    });
+
+    it([
+      `should return 42 for the following test input:`,
+      '          [ E 21 ]',
+      '          [ L 90 ]',
+      '          [ F 21 ]'
+    ].join(''), () => {
+      const testInput: Instruction[] = [
+        [ 'E', 21 ],
+        [ 'L', 90 ],
+        [ 'F', 21 ],
+      ];
+      const solution = solverPart1(testInput);
+
+      assert.strictEqual(solution, 42);
+    });
+
+    it([
+      `should return 0 for the following test input:`,
+      '          [ E 21 ]',
+      '          [ L 180 ]',
+      '          [ F 21 ]'
+    ].join(''), () => {
+      const testInput: Instruction[] = [
+        [ 'E', 21 ],
+        [ 'L', 180 ],
+        [ 'F', 21 ],
+      ];
+      const solution = solverPart1(testInput);
+
+      assert.strictEqual(solution, 0);
+    });
+
+    it([
+      `should return 42 for the following test input:`,
+      '          [ E 21 ]',
+      '          [ L 360 ]',
+      '          [ F 21 ]'
+    ].join(''), () => {
+      const testInput: Instruction[] = [
+        [ 'E', 21 ],
+        [ 'L', 360 ],
+        [ 'F', 21 ],
+      ];
+      const solution = solverPart1(testInput);
+
+      assert.strictEqual(solution, 42);
     });
   });
 });
