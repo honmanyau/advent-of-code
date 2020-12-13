2e81cae5ac452a150b13d78edeeeee9fdea0949b
honmanyau <honmanyau@users.noreply.github.com>
2020-12-13 16:44:22 +1100

    Added additional test cases that a simpler.

--- 2020/src/day-11/test.ts
+++ 2020/src/day-11/test.ts
@@ -19,17 +19,17 @@ describe('Day 11: Seating System (Part 1)', () => {
   describe(`evolve()`, () => {
     it([
       `should lead the following plan after one interation:`,
-      `#.##.##.##`,
-      `#######.##`,
-      `#.#.#..#..`,
-      `####.##.##`,
-      `#.##.##.##`,
-      `#.#####.##`,
-      `..#.#.....`,
-      `##########`,
-      `#.######.#`,
-      `#.#####.##`
-    ].join(''), () => {
+      `        #.##.##.##`,
+      `        #######.##`,
+      `        #.#.#..#..`,
+      `        ####.##.##`,
+      `        #.##.##.##`,
+      `        #.#####.##`,
+      `        ..#.#.....`,
+      `        ##########`,
+      `        #.######.#`,
+      `        #.#####.##`
+    ].join('\n'), () => {
       const solution = evolve(example).toString();
       const expected = [
         `#.##.##.##`,
@@ -48,18 +48,18 @@ describe('Day 11: Seating System (Part 1)', () => {
     });
 
     it([
-      `should lead the following plan after one interation:`,
-      `#.##.##.##`,
-      `#######.##`,
-      `#.#.#..#..`,
-      `####.##.##`,
-      `#.##.##.##`,
-      `#.#####.##`,
-      `..#.#.....`,
-      `##########`,
-      `#.######.#`,
-      `#.#####.##`
-    ].join(''), () => {
+      `should lead the following plan after 1 interation:`,
+      `        #.##.##.##`,
+      `        #######.##`,
+      `        #.#.#..#..`,
+      `        ####.##.##`,
+      `        #.##.##.##`,
+      `        #.#####.##`,
+      `        ..#.#.....`,
+      `        ##########`,
+      `        #.######.#`,
+      `        #.#####.##`
+    ].join('\n'), () => {
       const solution = evolve(example).toString();
       const expected = [
         `#.##.##.##`,
@@ -78,18 +78,18 @@ describe('Day 11: Seating System (Part 1)', () => {
     });
 
     it([
-      `should lead the following plan after one interation:`,
-      `#.LL.L#.##`,
-      `#LLLLLL.L#`,
-      `L.L.L..L..`,
-      `#LLL.LL.L#`,
-      `#.LL.LL.LL`,
-      `#.LLLL#.##`,
-      `..L.L.....`,
-      `#LLLLLLLL#`,
-      `#.LLLLLL.L`,
-      `#.#LLLL.##`
-    ].join(''), () => {
+      `should lead the following plan after 2 interation:`,
+      `        #.LL.L#.##`,
+      `        #LLLLLL.L#`,
+      `        L.L.L..L..`,
+      `        #LLL.LL.L#`,
+      `        #.LL.LL.LL`,
+      `        #.LLLL#.##`,
+      `        ..L.L.....`,
+      `        #LLLLLLLL#`,
+      `        #.LLLLLL.L`,
+      `        #.#LLLL.##`
+    ].join('\n'), () => {
       const solution = evolve(example, 2).toString();
       const expected = [
         `#.LL.L#.##`,
@@ -108,18 +108,18 @@ describe('Day 11: Seating System (Part 1)', () => {
     });
 
     it([
-      `should lead the following plan after one interation:`,
-      `#.##.L#.##`,
-      `#L###LL.L#`,
-      `L.#.#..#..`,
-      `#L##.##.L#`,
-      `#.##.LL.LL`,
-      `#.###L#.##`,
-      `..#.#.....`,
-      `#L######L#`,
-      `#.LL###L.L`,
-      `#.#L###.##`
-    ].join(''), () => {
+      `should lead the following plan after 3 interation:`,
+      `        #.##.L#.##`,
+      `        #L###LL.L#`,
+      `        L.#.#..#..`,
+      `        #L##.##.L#`,
+      `        #.##.LL.LL`,
+      `        #.###L#.##`,
+      `        ..#.#.....`,
+      `        #L######L#`,
+      `        #.LL###L.L`,
+      `        #.#L###.##`
+    ].join('\n'), () => {
       const solution = evolve(example, 3).toString();
       const expected = [
         `#.##.L#.##`,
@@ -138,18 +138,18 @@ describe('Day 11: Seating System (Part 1)', () => {
     });
 
     it([
-      `should lead the following plan after one interation:`,
-      `#.#L.L#.##`,
-      `#LLL#LL.L#`,
-      `L.L.L..#..`,
-      `#LLL.##.L#`,
-      `#.LL.LL.LL`,
-      `#.LL#L#.##`,
-      `..L.L.....`,
-      `#L#LLLL#L#`,
-      `#.LLLLLL.L`,
-      `#.#L#L#.##`
-    ].join(''), () => {
+      `should lead the following plan after 4 interation:`,
+      `        #.#L.L#.##`,
+      `        #LLL#LL.L#`,
+      `        L.L.L..#..`,
+      `        #LLL.##.L#`,
+      `        #.LL.LL.LL`,
+      `        #.LL#L#.##`,
+      `        ..L.L.....`,
+      `        #L#LLLL#L#`,
+      `        #.LLLLLL.L`,
+      `        #.#L#L#.##`
+    ].join('\n'), () => {
       const solution = evolve(example, 4).toString();
       const expected = [
         `#.#L.L#.##`,
@@ -168,19 +168,49 @@ describe('Day 11: Seating System (Part 1)', () => {
     });
 
     it([
-      `should lead the following plan after one interation:`,
-      `#.#L.L#.##`,
-      `#LLL#LL.L#`,
-      `L.#.L..#..`,
-      `#L##.##.L#`,
-      `#.#L.LL.LL`,
-      `#.#L#L#.##`,
-      `..L.L.....`,
-      `#L#L##L#L#`,
-      `#.LLLLLL.L`,
-      `#.#L#L#.##`
-    ].join(''), () => {
+      `should lead the following plan after 5 interation:`,
+      `        #.#L.L#.##`,
+      `        #LLL#LL.L#`,
+      `        L.#.L..#..`,
+      `        #L##.##.L#`,
+      `        #.#L.LL.LL`,
+      `        #.#L#L#.##`,
+      `        ..L.L.....`,
+      `        #L#L##L#L#`,
+      `        #.LLLLLL.L`,
+      `        #.#L#L#.##`
+    ].join('\n'), () => {
       const solution = evolve(example, 5).toString();
+      const expected = [
+        `#.#L.L#.##`,
+        `#LLL#LL.L#`,
+        `L.#.L..#..`,
+        `#L##.##.L#`,
+        `#.#L.LL.LL`,
+        `#.#L#L#.##`,
+        `..L.L.....`,
+        `#L#L##L#L#`,
+        `#.LLLLLL.L`,
+        `#.#L#L#.##`
+      ].map(processLine).toString();
+      
+      assert.strictEqual(solution, expected);
+    });
+
+    it([
+      `should lead the following plan after 10 interation:`,
+      `        #.#L.L#.##`,
+      `        #LLL#LL.L#`,
+      `        L.#.L..#..`,
+      `        #L##.##.L#`,
+      `        #.#L.LL.LL`,
+      `        #.#L#L#.##`,
+      `        ..L.L.....`,
+      `        #L#L##L#L#`,
+      `        #.LLLLLL.L`,
+      `        #.#L#L#.##`
+    ].join('\n'), () => {
+      const solution = evolve(example, 10).toString();
       const expected = [
         `#.#L.L#.##`,
         `#LLL#LL.L#`,
@@ -200,9 +230,75 @@ describe('Day 11: Seating System (Part 1)', () => {
 
   describe(`sovlerPart1()`, () => {
     it([
-      `should do something.`
+      `should return 37 for the example.`
     ].join(''), () => {
       const solution = solverPart1(example);
+
+      assert.strictEqual(solution, 37);
+    });
+
+    it([
+      `should return 1 for the following layout:`,
+      '           ...',
+      '           .L.',
+      '           ...'
+    ].join('\n'), () => {
+      const testInput = [
+        '...',
+        '.L.',
+        '...'
+      ].map(processLine);
+      const solution = solverPart1(testInput);
+
+      assert.strictEqual(solution, 1);
+    });
+
+    it([
+      `should return 1 for the following layout:`,
+      '           ...',
+      '           .#.',
+      '           ...'
+    ].join(''), () => {
+      const testInput = [
+        '...',
+        '.#.',
+        '...'
+      ].map(processLine);
+      const solution = solverPart1(testInput);
+
+      assert.strictEqual(solution, 1);
+    });
+
+    it([
+      `should return 4 for the following layout:`,
+      '           #.#',
+      '           .#.',
+      '           #.#'
+    ].join('\n'), () => {
+      const testInput = [
+        '#.#',
+        '.#.',
+        '#.#'
+      ].map(processLine);
+      const solution = solverPart1(testInput);
+
+      assert.strictEqual(solution, 4);
+    });
+
+    it([
+      `should return 4 for the following layout:`,
+      '           .#.',
+      '           ###',
+      '           .#.'
+    ].join('\n'), () => {
+      const testInput = [
+        '.#.',
+        '###',
+        '.#.'
+      ].map(processLine);
+      const solution = solverPart1(testInput);
+
+      assert.strictEqual(solution, 4);
     });
   });
 });
