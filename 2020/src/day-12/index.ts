0d93bc53eeafd2edb10e86d82c54c94d433eb2c4
honmanyau <honmanyau@users.noreply.github.com>
2020-12-13 23:14:32 +1100

    Implemented navigate().

--- 2020/src/day-12/index.ts
+++ 2020/src/day-12/index.ts
@@ -23,6 +23,7 @@ if (process.env.SOLVE && process.env.SOLVE.toLowerCase() === 'true') {
 // == Interfaces ==
 // ================
 export type Instruction = [ string, number ];
+export type Direction = 'N' | 'E' | 'S' | 'W';
 
 
 // ===============
@@ -66,4 +67,41 @@ export function solverPart1(instructions: [ string, number ][]) {
  */
 export function solverPart2(instructions: [ string, number ][]) {
   return -1E16;
+}
+
+/**
+ * This function takes the current direction and an `Instrution` as arguments
+ * and return the new direction and distance that one should travel.
+ * @param {Direction} direction The current direction of the ship.
+ * @param {Instruction} instruction A set of navigation instructions.
+ * @returns {Instruction} Instruciton for the direction and distance to travel.
+ */
+export function navigate(direction: Direction, instruction: Instruction) {
+  const [ steer, magnitude ] = instruction;
+  const compass = [ 'N', 'E', 'S', 'W' ];
+  
+  switch(steer) {
+    case 'F':
+      return [ direction, magnitude ];
+    case 'N':
+    case 'E':
+    case 'S':
+    case 'W':
+      return [ steer, magnitude ];
+    case 'L':
+    case 'R':
+      if (magnitude % 90 !== 0) {
+        throw Error('Magnitude for turning is not divisible by 90!');
+      }
+
+      const l = compass.length;
+      const currentIndex = compass.indexOf(steer);
+      const turns = (magnitude / 90);
+      const modifier = (steer === 'L') ? -1 : 1;
+      const newIndex = (((currentIndex + modifier * turns) % l) + l) % l;
+
+      return [ compass[newIndex], 0 ];
+    default:
+      throw Error('Something went horribly wrong!');
+  }
 }
