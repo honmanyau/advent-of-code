e2b2658d0b622ada1c222747f14f875700baa952
honmanyau <honmanyau@users.noreply.github.com>
2020-12-13 23:01:06 +1100

    Added tests for a nagivate() function.

--- 2020/src/day-12/test.ts
+++ 2020/src/day-12/test.ts
@@ -5,6 +5,7 @@ import * as path from 'path';
 import { yellow } from '../utilities';
 import {
   processFile,
+  navigate,
   solverPart1,
   solverPart2,
   Instruction
@@ -16,6 +17,62 @@ const exampleFile = fs.readFileSync(examplePathname, 'utf-8');
 const example = processFile(exampleFile);
 
 describe('Day 12: Rain Risk (Part 1)', () => {
+  describe(`navigate()`, () => {
+    it([
+      `should return [ 'E', 42 ] if the instructions, starting facing 'E', are:`,
+      `          [ 'E', 42 ]`
+    ].join('\n'), () => {
+      const solution = navigate('E', [ 'E', 42 ]);
+
+      assert.deepStrictEqual(solution, [ 'E', 42 ]);
+    });
+
+    it([
+      `should return [ 'E', 42 ] if the instructions, starting facing 'E', are:`,
+      `          [ 'F', 42 ]`
+    ].join('\n'), () => {
+      const solution = navigate('E', [ 'F', 42 ]);
+
+      assert.deepStrictEqual(solution, [ 'E', 42 ]);
+    });
+
+    it([
+      `should return [ 'S', 42 ] if the instructions, starting facing 'E', are:`,
+      `          [ 'S', 42 ]`
+    ].join('\n'), () => {
+      const solution = navigate('E', [ 'S', 42 ]);
+
+      assert.deepStrictEqual(solution, [ 'S', 42 ]);
+    });
+
+    it([
+      `should return [ 'S', 42 ] if the instructions, starting facing 'N', are:`,
+      `          [ 'S', 42 ]`
+    ].join('\n'), () => {
+      const solution = navigate('N', [ 'S', 42 ]);
+
+      assert.deepStrictEqual(solution, [ 'S', 42 ]);
+    });
+
+    it([
+      `should return [ 'S', 0 ] if the instructions, starting facing 'E', are:`,
+      `          [ 'R', 90 ]`
+    ].join('\n'), () => {
+      const solution = navigate('E', [ 'R', 0 ]);
+
+      assert.deepStrictEqual(solution, [ 'S', 42 ]);
+    });
+
+    it([
+      `should return [ 'N', 0 ] if the instructions, starting facing 'E', are:`,
+      `          [ 'L', 90 ]`
+    ].join('\n'), () => {
+      const solution = navigate('E', [ 'L', 90 ]);
+
+      assert.deepStrictEqual(solution, [ 'N', 0 ]);
+    });
+  });
+  
   describe(`sovlerPart1()`, () => {
     it([
       `should return 25 for the example.`
@@ -28,7 +85,7 @@ describe('Day 12: Rain Risk (Part 1)', () => {
     it([
       `should return 0 for the following test input:`,
       '          [ F 0 ]'
-    ].join(''), () => {
+    ].join('\n'), () => {
       const testInput: Instruction[] = [
         [ 'F', 0 ]
       ];
@@ -40,7 +97,7 @@ describe('Day 12: Rain Risk (Part 1)', () => {
     it([
       `should return 10 for the following test input:`,
       '          [ F 21 ]'
-    ].join(''), () => {
+    ].join('\n'), () => {
       const testInput: Instruction[] = [
         [ 'F', 21 ]
       ];
@@ -53,7 +110,7 @@ describe('Day 12: Rain Risk (Part 1)', () => {
       `should return 42 for the following test input:`,
       '          [ E 21 ]',
       '          [ S 21 ]'
-    ].join(''), () => {
+    ].join('\n'), () => {
       const testInput: Instruction[] = [
         [ 'E', 21 ],
         [ 'S', 21 ]
@@ -67,7 +124,7 @@ describe('Day 12: Rain Risk (Part 1)', () => {
       `should return 42 for the following test input:`,
       '          [ E 21 ]',
       '          [ N 21 ]'
-    ].join(''), () => {
+    ].join('\n'), () => {
       const testInput: Instruction[] = [
         [ 'E', 21 ],
         [ 'N', 21 ]
@@ -81,7 +138,7 @@ describe('Day 12: Rain Risk (Part 1)', () => {
       `should return 42 for the following test input:`,
       '          [ W 21 ]',
       '          [ S 21 ]'
-    ].join(''), () => {
+    ].join('\n'), () => {
       const testInput: Instruction[] = [
         [ 'W', 21 ],
         [ 'S', 21 ]
@@ -95,7 +152,7 @@ describe('Day 12: Rain Risk (Part 1)', () => {
       `should return 42 for the following test input:`,
       '          [ W 21 ]',
       '          [ N 21 ]'
-    ].join(''), () => {
+    ].join('\n'), () => {
       const testInput: Instruction[] = [
         [ 'W', 21 ],
         [ 'N', 21 ]
@@ -110,7 +167,7 @@ describe('Day 12: Rain Risk (Part 1)', () => {
       '          [ E 21 ]',
       '          [ L 90 ]',
       '          [ F 21 ]'
-    ].join(''), () => {
+    ].join('\n'), () => {
       const testInput: Instruction[] = [
         [ 'E', 21 ],
         [ 'L', 90 ],
@@ -126,7 +183,7 @@ describe('Day 12: Rain Risk (Part 1)', () => {
       '          [ E 21 ]',
       '          [ L 180 ]',
       '          [ F 21 ]'
-    ].join(''), () => {
+    ].join('\n'), () => {
       const testInput: Instruction[] = [
         [ 'E', 21 ],
         [ 'L', 180 ],
@@ -142,7 +199,7 @@ describe('Day 12: Rain Risk (Part 1)', () => {
       '          [ E 21 ]',
       '          [ L 360 ]',
       '          [ F 21 ]'
-    ].join(''), () => {
+    ].join('\n'), () => {
       const testInput: Instruction[] = [
         [ 'E', 21 ],
         [ 'L', 360 ],
