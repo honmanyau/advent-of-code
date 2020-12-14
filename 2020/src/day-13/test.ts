eccfa999a6983fe6dd4564d12a6714729e0c27f5
honmanyau <honmanyau@users.noreply.github.com>
2020-12-14 14:53:52 +1100

    Added tests for solverPart1().

--- 2020/src/day-13/test.ts
+++ 2020/src/day-13/test.ts
@@ -17,9 +17,35 @@ const example = processFile(exampleFile);
 describe('Day 13: Shuttle Search (Part 1)', () => {
   describe(`sovlerPart1()`, () => {
     it([
-      `should do something.`
+      `should return 295 for the example.`
     ].join(''), () => {
       const solution = solverPart1(example);
+
+      assert.strictEqual(solution, 295);
+    });
+
+    it([
+      `should return 0 for the test input [ 8, '7,x,4' ].`
+    ].join(''), () => {
+      const solution = solverPart1(example);
+
+      assert.strictEqual(solution, 0);
+    });
+
+    it([
+      `should return 12 for the test input [ 9, '7,x,4' ].`
+    ].join(''), () => {
+      const solution = solverPart1(example);
+
+      assert.strictEqual(solution, 12);
+    });
+
+    it([
+      `should return 22 for the test input [ 9, '7,11,x,4' ].`
+    ].join(''), () => {
+      const solution = solverPart1(example);
+
+      assert.strictEqual(solution, 22);
     });
   });
 });
