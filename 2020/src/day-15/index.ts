dcc6858acad1b0cca6c9d77d3f6533b7ce856909
honmanyau <honmanyau@users.noreply.github.com>
2020-12-15 21:39:29 +1100

    Not sure what I was thinking earlier: last turn is always i - 1, so it's not necessary to store two numbers for each spoken number. Sped up only by 25%, not happy.

--- 2020/src/day-15/index.ts
+++ 2020/src/day-15/index.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-import { logDuration } from '../utilities';
+import { green, logDuration } from '../utilities';
 
 
 if (process.env.SOLVE && process.env.SOLVE.toLowerCase() === 'true') {
@@ -66,35 +66,69 @@ export function solverPart2(input: number[]) {
  * @param {number} turns The number of turns to simulate the game for.
  */
 export function play(input, turns) {
-  if (turns <= input.length) {
-    return input[turns - 1];
-  }
-
   const memory = {};
-  let lastSpoken = -1;
+  let lastSpoken = input[0];
 
-  lastSpoken = input[0];
+  for (let i = 1; i < input.length && i < turns; i++) {
+    const key = `o${lastSpoken}`;
+    const lastTurn = i - 1;
 
-  for (let i = 1; i < input.length; i++) {
-    const key = `m${lastSpoken}`;
-
-    memory[key] = [ null, i - 1 ];
-
-    lastSpoken = input[i];
+    memory[key] = lastTurn;
+    lastSpoken = input[i]; 
   }
-    
-  for (let i = 3; i < turns; i++) {
-    const key = `m${lastSpoken}`;
 
-    if (!memory[key]) {
-      memory[key] = [ null, i - 1 ];
+  for (let i = input.length; i < turns; i++) {
+    const key = `o${lastSpoken}`;
+    const secondLastTurn = memory[key];
+    const lastTurn = i - 1;
+
+    if (secondLastTurn === undefined) {
+      memory[key] = lastTurn;
 
       lastSpoken = 0;
     }
     else {
-      memory[key][0] = memory[key][1];
-      memory[key][1] = (i - 1);
-      lastSpoken = memory[key][1] - memory[key][0];
+      memory[key] = lastTurn;
+      lastSpoken = lastTurn - secondLastTurn;
+    }
+  }
+
+  return lastSpoken;
+}
+
+/**
+ * This funtion plays the Elves' game for a given number of turns.
+ * @param {number[]} input The starting numbers being spoken.
+ * @param {number} turns The number of turns to simulate the game for.
+ */
+export function playAttempt1(input, turns) {
+  const memory = {};
+  let lastSpoken = -1;
+
+  lastSpoken = input[0];
+    
+  for (let i = 1; i < turns; i++) {
+    if (i < input.length) {
+      memory[lastSpoken] = [ i - 1 ];
+
+      lastSpoken = input[i];
+    }
+    else {
+      const lastSpokenMemory = memory[lastSpoken];
+
+      if (!lastSpokenMemory) {
+        memory[lastSpoken] = [ i - 1 ];
+
+        lastSpoken = 0;
+      }
+      else {
+        lastSpokenMemory.push(i - 1);
+        
+        const lastTwoTurns = lastSpokenMemory.slice(-2);
+        const difference = lastTwoTurns[1] - lastTwoTurns[0];
+
+        lastSpoken = difference;
+      }
     }
   }
 
