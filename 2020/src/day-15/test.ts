ef4c37eadf4522a6ff161de686f4e15eec8a1373
honmanyau <honmanyau@users.noreply.github.com>
2020-12-15 17:07:54 +1100

    Cleaned up comment.

--- 2020/src/day-15/test.ts
+++ 2020/src/day-15/test.ts
@@ -165,17 +165,6 @@ describe('Day 15: Rambunctious Recitation (Part 1)', () => {
   });
 });
 
-/*
-
-    Given the starting numbers 1,3,2, the 2020th number spoken is 1.
-    Given the starting numbers 2,1,3, the 2020th number spoken is 10.
-    Given the starting numbers 1,2,3, the 2020th number spoken is 27.
-    Given the starting numbers 2,3,1, the 2020th number spoken is 78.
-    Given the starting numbers 3,2,1, the 2020th number spoken is 438.
-    Given the starting numbers 3,1,2, the 2020th number spoken is 1836.
-
-*/
-
 describe('Day 15: Rambunctious Recitation (Part 2)', () => {
   describe('solverPart2()', () => {
     // Given example.
