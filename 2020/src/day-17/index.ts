78bf1d46fe0d4c21d030c80ef07232c962602e07
honmanyau <honmanyau@users.noreply.github.com>
2020-12-23 03:00:26 +1100

    Implemented evolve().

--- 2020/src/day-17/index.ts
+++ 2020/src/day-17/index.ts
@@ -81,3 +81,163 @@ export function solverPart1(cube: Cube) {
 export function solverPart2(cube: Cube) {
   return -1;
 }
+
+/**
+ * This function evolves a Conway Cube for the given number of generations.
+ * @param {Cube} cube A Conway Cube.
+ * @param {number} generations The number of generations to evolve for.
+ * @returns {Cube} A Conway Cube.
+ */
+export function evolve(cube: Cube, generations: number) {
+  let newCube = JSON.parse(JSON.stringify(cube));;
+
+  for (let generation = 0; generation < generations; generation++) {
+    const zs = Object.keys(newCube).map(Number).sort((a, b) => a - b);
+    const ys = Object.keys(newCube[zs[0]]).map(Number).sort((a, b) => a - b);
+    const xs = Object.keys(newCube[zs[0]][ys[0]]).map(Number).sort((a, b) => a - b);
+    let zActiveMin: number;
+    let zActiveMax: number;
+    let yActiveMin: number;
+    let yActiveMax: number;
+    let xActiveMin: number;
+    let xActiveMax: number;
+    let nextCube = JSON.parse(JSON.stringify(newCube));
+
+    for (let z = zs[0] - 1; z <= zs.slice(-1)[0] + 1; z++) {
+      for (let y = ys[0] - 1; y <= ys.slice(-1)[0] + 1; y++) {
+        for (let x = xs[0] - 1; x <= xs.slice(-1)[0] + 1; x++) {
+          const neighbours = getNeighbours(z, y, x);
+          const nodeMissing = newCube[z] === undefined
+              || newCube[z][y] === undefined
+              || newCube[z][y][x] === undefined;
+          let numActive = 0;
+  
+          for (const neighbour of neighbours) {
+            const [ zn, yn, xn ] = neighbour;
+            const neighbourexists = newCube[zn] !== undefined
+              && newCube[zn][yn] !== undefined
+              && newCube[zn][yn][xn] !== undefined;
+  
+            if (neighbourexists) {
+              if (newCube[zn][yn][xn] === '#') {
+                numActive++;
+              }
+            }
+          }
+
+          if (nodeMissing) {
+            nextCube[z] = nextCube[z] || {};
+            nextCube[z][y] = nextCube[z][y] || {};
+            nextCube[z][y][x] = '.';
+          }
+
+          if (nodeMissing || newCube[z][y][x] === '.') {
+            if (numActive === 3) {
+              nextCube[z][y][x] = '#';
+
+              zActiveMin = (zActiveMin === undefined || z < zActiveMin)
+                ? z
+                : zActiveMin;
+              yActiveMin = (yActiveMin === undefined || y < yActiveMin)
+                ? y
+                : yActiveMin;
+              xActiveMin = (xActiveMin === undefined || x < xActiveMin)
+                ? x
+                : xActiveMin;
+              zActiveMax = (zActiveMax === undefined || z > zActiveMax)
+                ? z
+                : zActiveMax;
+              yActiveMax = (yActiveMax === undefined || y > yActiveMax)
+                ? y
+                : yActiveMax;
+              xActiveMax = (xActiveMax === undefined || x > xActiveMax)
+                ? x
+                : xActiveMax;
+            }
+          }
+          else {
+            if (numActive !== 2 && numActive !== 3) {
+              nextCube[z][y][x] = '.';
+            }
+            else {
+              zActiveMin = (zActiveMin === undefined || z < zActiveMin)
+                ? z
+                : zActiveMin;
+              yActiveMin = (yActiveMin === undefined || y < yActiveMin)
+                ? y
+                : yActiveMin;
+              xActiveMin = (xActiveMin === undefined || x < xActiveMin)
+                ? x
+                : xActiveMin;
+              zActiveMax = (zActiveMax === undefined || z > zActiveMax)
+                ? z
+                : zActiveMax;
+              yActiveMax = (yActiveMax === undefined || y > yActiveMax)
+                ? y
+                : yActiveMax;
+              xActiveMax = (xActiveMax === undefined || x > xActiveMax)
+                ? x
+                : xActiveMax;
+            }
+          }
+        }
+      }
+    }
+
+    // Clean up empty space.
+    for (const zKey in nextCube) {
+      const z = Number(zKey);
+
+      if (z < zActiveMin || z > zActiveMax) {
+        delete nextCube[z];
+        continue;
+      }
+
+      for (const yKey in nextCube[zKey]) {
+        const y = Number(yKey);
+
+        if (y < yActiveMin || y > yActiveMax) {
+          delete nextCube[z][y];
+          continue
+        }
+
+        for (const xKey in nextCube[zKey][yKey]) {
+          const x = Number(xKey);
+
+          if (x < xActiveMin || x > xActiveMax) {
+            delete nextCube[z][y][x];
+            continue;
+          }
+        }
+      }
+    }
+
+    newCube = nextCube;
+  }
+
+  return newCube;
+}
+
+/**
+ * This function generates the coordinates of all the neighbours of a given
+ * point in a Conway Cube.
+ * @param {number} z The z coordinate of a point.
+ * @param {number} y The y coordinate of a point.
+ * @param {number} x The x coordinate of a point.
+ * @returns {[ number, number, number ][]} The coordinates of the neighbours.
+ */
+export function getNeighbours(z, y, x): [ number, number, number ][] {
+  const neighbours = [];
+
+  for (let i = z - 1; i <= z + 1; i++) {
+    for (let j = y - 1; j <= y + 1; j++) {
+      for (let k = x - 1; k <= x + 1; k++) {
+        if (!(i === z && j === y && k === x)) {
+          neighbours.push([ i, j, k ]);
+        }
+      }
+    }
+  }
+
+  return neighbours;
+}
