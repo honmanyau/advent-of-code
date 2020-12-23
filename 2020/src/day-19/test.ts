ddb288db1086335147d40d207908cc1a4f2c9f5c
honmanyau <honmanyau@users.noreply.github.com>
2020-12-23 23:05:51 +1100

    Added one more test for indices with a single reference.

--- 2020/src/day-19/test.ts
+++ 2020/src/day-19/test.ts
@@ -42,7 +42,7 @@ describe('Day 19: Monster Messages (Part 1)', () => {
 
     it([
       `should return 7 if the messages in the input are changed to the valid`,
-      ` permations in the example except for the last entry.`
+      ` permations in the example except for the last entry`
     ].join(''), () => {
       const exampleCopy = JSON.parse(exampleString);
 
@@ -56,7 +56,7 @@ describe('Day 19: Monster Messages (Part 1)', () => {
 
     it([
       `should return 6 if the messages in the input are changed to the valid`,
-      ` permations in the example except for the first and last entry.`
+      ` permations in the example except for the first and last entry`
     ].join(''), () => {
       const exampleCopy = JSON.parse(exampleString);
 
@@ -66,6 +66,28 @@ describe('Day 19: Monster Messages (Part 1)', () => {
       const solution = solverPart1(exampleCopy);
 
       assert.strictEqual(solution, 6);
+    });
+
+    it([
+      `should return 4 for a test input containing a rule that is linked to`,
+      ` only one other rule`
+    ].join(''), () => {
+      const exampleCopy = processFile([
+        `0: 4 1 5`,
+        `1: 2 3 | 3 2`,
+        `2: 4 4 | 5 5`,
+        `3: 2`,
+        `4: "a"`,
+        `5: "b"`,
+        ``,
+        `aaaaab`,
+        `aaabbb`,
+        `abbaab`,
+        `abbbbb`,
+      ].join('\n'));
+      const solution = solverPart1(exampleCopy);
+
+      assert.strictEqual(solution, 4);
     });
   });
 });
