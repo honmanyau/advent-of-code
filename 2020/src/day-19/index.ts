0d590e2db9348327e4fa7b25d2a6e8645b9cc5fe
honmanyau <honmanyau@users.noreply.github.com>
2020-12-23 20:25:07 +1100

    Advent of Code 2020, Day 19, project setup.

---
+++ 2020/src/day-19/index.ts
@@ -0,0 +1,68 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+import { green } from '../utilities';
+
+
+if (process.env.SOLVE && process.env.SOLVE.toLowerCase() === 'true') {
+  const challengePathname = path.resolve(__dirname, './input.txt');
+  const challengeFile = fs.readFileSync(challengePathname, 'utf-8');
+  const challenge = processFile(challengeFile);
+  const solutionPart1 = solverPart1(challenge);
+  const solutionPart2 = solverPart2(challenge);
+
+  console.log([
+    `The solutions for 2020's "Day 19: Monster Messages" are:`,
+    `  * Part 1: ${green(solutionPart1)}`,
+    `  * Part 2: ${green(solutionPart2)}`
+  ].join('\n'));
+}
+
+
+// ================
+// == Interfaces ==
+// ================
+
+
+// ===============
+// == Functions ==
+// ===============
+/**
+ * This function uses the `processEntry` function process an input file of
+ * the Advent of Code 2020's "Day 19: Monster Messages" challenge.
+ * @param {string} file A challenge file read in as a string.
+ * @returns {string[]} An array where each line is an entry of the challenge.
+ */
+export function processFile(file: string) {
+  return file.trim().split('\n').map(processEntry);
+}
+
+/**
+ * This function processes each entry of pre-processed input.
+ * the Advent of Code 2020's "Day 19: Monster Messages" challenge.
+ * @param {string} file A challenge file read in as a string.
+ * @returns {string} An array where each line is an entry of the challenge.
+ */
+export function processEntry(entry: string) {
+  return entry;
+}
+
+/**
+ * The solver function for Part 1 of the Advent of Code 2020's
+ * "Day 19: Monster Messages" challenge.
+ * @param {string[]} input Entries of the challenge.
+ * @returns {number} Number of valid entries.
+ */
+export function solverPart1(input: string[]) {
+  return -1;
+}
+
+/**
+ * The solver function for Part 2 of the Advent of Code 2020's
+ * "Day 19: Monster Messages" challenge.
+ * @param {string[]} input Entries of the challenge.
+ * @returns {number} Number of valid entries.
+ */
+export function solverPart2(input: string[]) {
+  return -1;
+}
