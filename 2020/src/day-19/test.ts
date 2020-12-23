6c22339aee844fdb727d4fe557dd4c652fc3291e
honmanyau <honmanyau@users.noreply.github.com>
2020-12-24 00:47:53 +1100

    Renamed files to import.

--- 2020/src/day-19/test.ts
+++ 2020/src/day-19/test.ts
@@ -2,7 +2,6 @@ import * as assert from 'assert';
 import * as fs from 'fs';
 import * as path from 'path';
 
-import { yellow } from '../utilities';
 import {
   processFile,
   solverPart1,
@@ -10,17 +9,21 @@ import {
 } from './index';
 
 
-const examplePathname = path.resolve(__dirname, './example.txt');
-const exampleFile = fs.readFileSync(examplePathname, 'utf-8');
-const example = processFile(exampleFile);
-const exampleString = JSON.stringify(example);
+const examplePathnamePart1 = path.resolve(__dirname, './example-part-1.txt');
+const examplePathnamePart2 = path.resolve(__dirname, './example-part-2.txt');
+const exampleFilePart1 = fs.readFileSync(examplePathnamePart1, 'utf-8');
+const exampleFilePart2 = fs.readFileSync(examplePathnamePart2, 'utf-8');
+const examplePart1 = processFile(exampleFilePart1);
+const examplePart2 = processFile(exampleFilePart2);
+const exampleStringPart1 = JSON.stringify(examplePart1);
+const exampleStringPart2 = JSON.stringify(examplePart2);
 
 describe('Day 19: Monster Messages (Part 1)', () => {
   describe(`sovlerPart1()`, () => {
     it([
       `should return 2 for the example.`
     ].join(''), () => {
-      const exampleCopy = JSON.parse(exampleString);
+      const exampleCopy = JSON.parse(exampleStringPart1);
       const solution = solverPart1(exampleCopy);
 
       assert.strictEqual(solution, 2);
@@ -30,7 +33,7 @@ describe('Day 19: Monster Messages (Part 1)', () => {
       `should return 8 if the messages in the input are changed to the valid`,
       ` permations in the example`
     ].join(''), () => {
-      const exampleCopy = JSON.parse(exampleString);
+      const exampleCopy = JSON.parse(exampleStringPart1);
 
       exampleCopy.messages =
         `aaaabb aaabab abbabb abbbab aabaab aabbbb abaaab ababbb`.split(' ');
@@ -44,7 +47,7 @@ describe('Day 19: Monster Messages (Part 1)', () => {
       `should return 7 if the messages in the input are changed to the valid`,
       ` permations in the example except for the last entry`
     ].join(''), () => {
-      const exampleCopy = JSON.parse(exampleString);
+      const exampleCopy = JSON.parse(exampleStringPart1);
 
       exampleCopy.messages =
         `aaaabb aaabab abbabb abbbab aabaab aabbbb abaaab bbbbbb`.split(' ');
@@ -58,7 +61,7 @@ describe('Day 19: Monster Messages (Part 1)', () => {
       `should return 6 if the messages in the input are changed to the valid`,
       ` permations in the example except for the first and last entry`
     ].join(''), () => {
-      const exampleCopy = JSON.parse(exampleString);
+      const exampleCopy = JSON.parse(exampleStringPart1);
 
       exampleCopy.messages =
         `aaaaaa aaabab abbabb abbbab aabaab aabbbb abaaab bbbbbb`.split(' ');
@@ -98,7 +101,8 @@ describe('Day 19: Monster Messages (Part 2)', () => {
     it([
       `should do something.`
     ].join(''), () => {
-      const solution = solverPart2(example);
+      const exampleCopy = JSON.parse(exampleStringPart2);
+      const solution = solverPart2(exampleCopy);
     });
   });
 });
