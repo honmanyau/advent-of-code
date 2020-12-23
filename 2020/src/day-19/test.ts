e0e7e18b7c5465307579227c66a76be1e52e6e9e
honmanyau <honmanyau@users.noreply.github.com>
2020-12-23 21:09:07 +1100

    Added tests for solverPart1().

--- 2020/src/day-19/test.ts
+++ 2020/src/day-19/test.ts
@@ -13,13 +13,59 @@ import {
 const examplePathname = path.resolve(__dirname, './example.txt');
 const exampleFile = fs.readFileSync(examplePathname, 'utf-8');
 const example = processFile(exampleFile);
+const exampleString = JSON.stringify(example);
 
 describe('Day 19: Monster Messages (Part 1)', () => {
   describe(`sovlerPart1()`, () => {
     it([
-      `should do something.`
+      `should return 2 for the example.`
     ].join(''), () => {
-      const solution = solverPart1(example);
+      const exampleCopy = JSON.parse(exampleString);
+      const solution = solverPart1(exampleCopy);
+
+      assert.strictEqual(solution, 2);
+    });
+
+    it([
+      `should return 8 if the messages in the input are changed to the valid`,
+      ` permations in the example`
+    ].join(''), () => {
+      const exampleCopy = JSON.parse(exampleString);
+
+      exampleCopy.messages =
+        `aaaabb aaabab abbabb abbbab aabaab aabbbb abaaab ababbb`.split(' ');
+
+      const solution = solverPart1(exampleCopy);
+
+      assert.strictEqual(solution, 8);
+    });
+
+    it([
+      `should return 7 if the messages in the input are changed to the valid`,
+      ` permations in the example except for the last entry.`
+    ].join(''), () => {
+      const exampleCopy = JSON.parse(exampleString);
+
+      exampleCopy.messages =
+        `aaaabb aaabab abbabb abbbab aabaab aabbbb abaaab bbbbbb`.split(' ');
+
+      const solution = solverPart1(exampleCopy);
+
+      assert.strictEqual(solution, 7);
+    });
+
+    it([
+      `should return 6 if the messages in the input are changed to the valid`,
+      ` permations in the example except for the first and last entry.`
+    ].join(''), () => {
+      const exampleCopy = JSON.parse(exampleString);
+
+      exampleCopy.messages =
+        `aaaaaa aaabab abbabb abbbab aabaab aabbbb abaaab bbbbbb`.split(' ');
+
+      const solution = solverPart1(exampleCopy);
+
+      assert.strictEqual(solution, 6);
     });
   });
 });
